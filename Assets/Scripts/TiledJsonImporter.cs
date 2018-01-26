4549a1ab3c6855cc3335f92a51f3c3909db1a474
Jonas Kunze <kunze.jonas@gmail.com>
2018-01-26 22:13:15 +0100

    Implemented simple Tiled Json importer

---
+++ Assets/Scripts/TiledJsonImporter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Level
+{
+    public int height;
+    public bool infinite;
+    public Layer[] layers;
+}
+
+[Serializable]
+public class Layer
+{
+    int[] data;
+    public int height;
+    public string name;
+    public float opacity;
+    public string type;
+    public bool visible;
+    public int width;
+    public int x;
+    public int y;
+}
+
+public class TiledJsonImporter : MonoBehaviour
+{
+    public string fileName = "level.json";
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        string json = System.IO.File.ReadAllText(fileName);
+        Level level = JsonUtility.FromJson<Level>(json);	
+    }
+}
